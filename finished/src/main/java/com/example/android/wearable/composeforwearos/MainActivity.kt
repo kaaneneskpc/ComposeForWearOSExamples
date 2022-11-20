dd6e5e62a8ed8a9689cca5886f392ed9dc07e898
Kaan Enes KAPICI <kaaneneskpc1@gmail.com>
2022-11-20 12:22:29 +0300

    Refactoring Code

--- finished/src/main/java/com/example/android/wearable/composeforwearos/MainActivity.kt
+++ finished/src/main/java/com/example/android/wearable/composeforwearos/MainActivity.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.wearable.composeforwearos
 
 import android.os.Bundle
@@ -23,11 +8,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.AutoCenteringParams
 import androidx.wear.compose.material.PositionIndicator
@@ -40,19 +23,6 @@ import androidx.wear.compose.material.rememberScalingLazyListState
 import androidx.wear.compose.material.scrollAway
 import com.example.android.wearable.composeforwearos.theme.WearAppTheme
 
-/**
- * This code lab is meant to help existing Compose developers get up to speed quickly on
- * Compose for Wear OS.
- *
- * The code lab walks through a majority of the simple composables for Wear OS (both similar to
- * existing mobile composables and new composables).
- *
- * It also covers more advanced composables like [ScalingLazyColumn] (Wear OS's version of
- * [LazyColumn]) and the Wear OS version of [Scaffold].
- *
- * Check out [this link](https://android-developers.googleblog.com/2021/10/compose-for-wear-os-now-in-developer.html)
- * for more information on Compose for Wear OS.
- */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,18 +36,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WearApp() {
     WearAppTheme {
-        // TODO: Swap to ScalingLazyListState
         val listState = rememberScalingLazyListState()
 
-        /* *************************** Part 4: Wear OS Scaffold *************************** */
-        // TODO (Start): Create a Scaffold (Wear Version)
         Scaffold(
             timeText = {
                 TimeText(modifier = Modifier.scrollAway(listState))
             },
             vignette = {
-                // Only show a Vignette for scrollable screens. This code lab only has one screen,
-                // which is scrollable, so we show it all the time.
                 Vignette(vignettePosition = VignettePosition.TopAndBottom)
             },
             positionIndicator = {
@@ -87,7 +52,6 @@ fun WearApp() {
             }
         ) {
 
-            // Modifiers used by our Wear composables.
             val contentModifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 8.dp)
@@ -95,39 +59,18 @@ fun WearApp() {
                 .size(24.dp)
                 .wrapContentSize(align = Alignment.Center)
 
-            /* *************************** Part 3: ScalingLazyColumn *************************** */
-            // TODO: Swap a ScalingLazyColumn (Wear's version of LazyColumn)
             ScalingLazyColumn(
                 modifier = Modifier.fillMaxSize(),
                 autoCentering = AutoCenteringParams(itemIndex = 0),
                 state = listState
             ) {
-
-                /* ******************* Part 1: Simple composables ******************* */
                 item { ButtonExample(contentModifier, iconModifier) }
                 item { TextExample(contentModifier) }
                 item { CardExample(contentModifier, iconModifier) }
-
-                /* ********************* Part 2: Wear unique composables ********************* */
                 item { ChipExample(contentModifier, iconModifier) }
                 item { ToggleChipExample(contentModifier) }
             }
-
-            // TODO (End): Create a Scaffold (Wear Version)
         }
     }
 }
 
-// Note: Preview in Android Studio doesn't support the round view yet (coming soon).
-@Preview(
-    widthDp = WEAR_PREVIEW_DEVICE_WIDTH_DP,
-    heightDp = WEAR_PREVIEW_DEVICE_HEIGHT_DP,
-    apiLevel = WEAR_PREVIEW_API_LEVEL,
-    uiMode = WEAR_PREVIEW_UI_MODE,
-    backgroundColor = WEAR_PREVIEW_BACKGROUND_COLOR_BLACK,
-    showBackground = WEAR_PREVIEW_SHOW_BACKGROUND
-)
-@Composable
-fun WearAppPreview() {
-    WearApp()
-}
--- finished/src/main/java/com/example/android/wearable/composeforwearos/ReusableComponents.kt
+++ finished/src/main/java/com/example/android/wearable/composeforwearos/ReusableComponents.kt
@@ -1,27 +1,6 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.wearable.composeforwearos
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Message
 import androidx.compose.material.icons.rounded.Phone
@@ -31,31 +10,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Switch
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.AppCard
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.Icon
-import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Switch
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.ToggleChip
-import androidx.wear.compose.material.ToggleChipDefaults
-import com.example.android.wearable.composeforwearos.theme.WearAppTheme
 
-/* Contains individual Wear OS demo composables for the code lab. */
-
-// TODO: Create a Button Composable (with a Row to center)
 @Composable
 fun ButtonExample(
     modifier: Modifier = Modifier,
@@ -65,11 +34,7 @@ fun ButtonExample(
         modifier = modifier,
         horizontalArrangement = Arrangement.Center
     ) {
-        // Button
-        Button(
-            modifier = Modifier.size(ButtonDefaults.LargeButtonSize),
-            onClick = { /* ... */ },
-        ) {
+        Button(onClick = {}, modifier = Modifier.size(ButtonDefaults.LargeButtonSize)) {
             Icon(
                 imageVector = Icons.Rounded.Phone,
                 contentDescription = "triggers phone action",
@@ -79,18 +44,19 @@ fun ButtonExample(
     }
 }
 
-// TODO: Create a Text Composable
 @Composable
-fun TextExample(modifier: Modifier = Modifier) {
+fun TextExample(
+    modifier: Modifier = Modifier,
+) {
     Text(
         modifier = modifier,
         textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.device_shape)
+        color = Color.White,
+        text = "Kaan Enes KAPICI"
     )
 }
 
-// TODO: Create a Card (specifically, an AppCard) Composable
+
 @Composable
 fun CardExample(
     modifier: Modifier = Modifier,
@@ -107,14 +73,14 @@ fun CardExample(
         },
         appName = { Text("Messages") },
         time = { Text("12m") },
-        title = { Text("Kim Green") },
+        title = { Text("Irem Ozturk") },
         onClick = { /* ... */ }
     ) {
         Text("On my way!")
     }
+
 }
 
-// TODO: Create a Chip Composable
 @Composable
 fun ChipExample(
     modifier: Modifier = Modifier,
@@ -140,7 +106,6 @@ fun ChipExample(
     )
 }
 
-// TODO: Create a ToggleChip Composable
 @Composable
 fun ToggleChipExample(modifier: Modifier = Modifier) {
     var checked by remember { mutableStateOf(true) }
@@ -166,150 +131,4 @@ fun ToggleChipExample(modifier: Modifier = Modifier) {
             )
         }
     )
-}
-
-// Function only used as a demo for when you start the code lab (removed as step 1).
-@Composable
-fun StartOnlyTextComposables() {
-    Text(
-        modifier = Modifier.fillMaxSize(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world_starter)
-    )
-}
-
-/* Previews of Composables. */
-// Note: Preview in Android Studio doesn't support the round view yet (coming soon).
-
-// Hello, world starter text preview
-@Preview(
-    group = "Starter",
-    widthDp = WEAR_PREVIEW_ELEMENT_WIDTH_DP,
-    heightDp = WEAR_PREVIEW_ELEMENT_HEIGHT_DP,
-    apiLevel = WEAR_PREVIEW_API_LEVEL,
-    uiMode = WEAR_PREVIEW_UI_MODE,
-    backgroundColor = WEAR_PREVIEW_BACKGROUND_COLOR_BLACK,
-    showBackground = WEAR_PREVIEW_SHOW_BACKGROUND
-)
-@Composable
-fun StartOnlyTextComposablesPreview() {
-    WearAppTheme {
-        StartOnlyTextComposables()
-    }
-}
-
-// Button Preview
-@Preview(
-    group = "Button",
-    widthDp = WEAR_PREVIEW_ELEMENT_WIDTH_DP,
-    heightDp = WEAR_PREVIEW_ELEMENT_HEIGHT_DP,
-    apiLevel = WEAR_PREVIEW_API_LEVEL,
-    uiMode = WEAR_PREVIEW_UI_MODE,
-    backgroundColor = WEAR_PREVIEW_BACKGROUND_COLOR_BLACK,
-    showBackground = WEAR_PREVIEW_SHOW_BACKGROUND
-)
-@Composable
-fun ButtonExamplePreview() {
-    WearAppTheme {
-        ButtonExample(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp),
-            iconModifier = Modifier
-                .size(24.dp)
-                .wrapContentSize(align = Alignment.Center)
-        )
-    }
-}
-
-// Text Preview
-@Preview(
-    group = "Text",
-    widthDp = WEAR_PREVIEW_ROW_WIDTH_DP,
-    heightDp = WEAR_PREVIEW_ROW_HEIGHT_DP,
-    apiLevel = WEAR_PREVIEW_API_LEVEL,
-    uiMode = WEAR_PREVIEW_UI_MODE,
-    backgroundColor = WEAR_PREVIEW_BACKGROUND_COLOR_BLACK,
-    showBackground = WEAR_PREVIEW_SHOW_BACKGROUND
-)
-@Composable
-fun TextExamplePreview() {
-    WearAppTheme {
-        TextExample(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
-        )
-    }
-}
-
-// Card Preview
-@Preview(
-    group = "Card",
-    widthDp = WEAR_PREVIEW_ROW_WIDTH_DP,
-    heightDp = WEAR_PREVIEW_ROW_HEIGHT_DP,
-    apiLevel = WEAR_PREVIEW_API_LEVEL,
-    uiMode = WEAR_PREVIEW_UI_MODE,
-    backgroundColor = WEAR_PREVIEW_BACKGROUND_COLOR_BLACK,
-    showBackground = WEAR_PREVIEW_SHOW_BACKGROUND
-)
-@Composable
-fun CardExamplePreview() {
-    WearAppTheme {
-        CardExample(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp),
-            iconModifier = Modifier
-                .size(24.dp)
-                .wrapContentSize(align = Alignment.Center)
-        )
-    }
-}
-
-
-// Chip Preview
-@Preview(
-    group = "Chip",
-    widthDp = WEAR_PREVIEW_ROW_WIDTH_DP,
-    heightDp = WEAR_PREVIEW_ROW_HEIGHT_DP,
-    apiLevel = WEAR_PREVIEW_API_LEVEL,
-    uiMode = WEAR_PREVIEW_UI_MODE,
-    backgroundColor = WEAR_PREVIEW_BACKGROUND_COLOR_BLACK,
-    showBackground = WEAR_PREVIEW_SHOW_BACKGROUND
-)
-@Composable
-fun ChipExamplePreview() {
-    WearAppTheme {
-        ChipExample(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp),
-            iconModifier = Modifier
-                .size(24.dp)
-                .wrapContentSize(align = Alignment.Center)
-        )
-    }
-}
-
-// Toggle Chip Preview
-@Preview(
-    group = "Toggle Chip",
-    widthDp = WEAR_PREVIEW_ROW_WIDTH_DP,
-    heightDp = WEAR_PREVIEW_ROW_HEIGHT_DP,
-    apiLevel = WEAR_PREVIEW_API_LEVEL,
-    uiMode = WEAR_PREVIEW_UI_MODE,
-    backgroundColor = WEAR_PREVIEW_BACKGROUND_COLOR_BLACK,
-    showBackground = WEAR_PREVIEW_SHOW_BACKGROUND
-)
-@Composable
-fun ToggleChipExamplePreview() {
-    WearAppTheme {
-        ToggleChipExample(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
-        )
-    }
 }
